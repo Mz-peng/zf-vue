088ff42c10eab55aad471265f8106da4dfc8f6af
maozp <1654331345@qq.com>
2022-08-13 08:57:39 +0800

    删除HelloWorld组件

--- vue-lesson-project/src/main.js
+++ vue-lesson-project/src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  // 渲染函数 重要
+  // 默认main文件中只支持render方法
+  render: (h) => h(App),
+}).$mount("#app");
