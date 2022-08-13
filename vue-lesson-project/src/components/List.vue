c832379f11e20edd99df26bb81c20330733e5c02
maozp <1654331345@qq.com>
2022-08-14 02:04:09 +0800

    扩展 iview Table 组件

--- vue-lesson-project/src/components/List.vue
+++ vue-lesson-project/src/components/List.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-
+    <ul>
+      <template v-if="!render">
+        <li v-for="l in list" :key="l">
+          {{ l }}
+        </li>
+      </template>
+      <template v-else v-for="l in list">
+        <List :render="render" :key="l" :a="l"></List>
+      </template>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import List from "./list.js";
+export default {
+  props: ["list", "render"],
+  components: {
+    List,
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
