23567bc7396bf0140e50b05cdc6c9eb57204a46f
maozp <1654331345@qq.com>
2022-08-13 16:18:24 +0800

    函数式组件

--- vue-lesson-project/src/App.vue
+++ vue-lesson-project/src/App.vue
@@ -1,14 +1,42 @@
 <template>
   <div id="app">
-    hello
+    <Mytitle :type="1" :fn="say">hello</Mytitle>
+    <List :list="arr" :render="render">
+
+    </List>
   </div>
 </template>
 
 <script>
+/* 
+  组件：
+  1. 声明
+  2. 引用
+  3. 注册
+*/
+// import MyTitle from './components/MyTitle.vue';
+import List from './components/List.vue';
 
+import Mytitle from './components/MyTitle';
 
 export default {
   name: 'App',
+  // 注册组件
+  components: {
+    Mytitle,
+    List
+  },
+  data: {
+    arr: [1, 2, 3, 4]
+  },
+  methods: {
+    say() {
+      alert(1)
+    },
+    render(h) {
+      return <h1></h1>
+    }
+  }
   
 }
 </script>
--- vue-lesson-project/vue.config.js
+++ vue-lesson-project/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  runtimeComplier: true,
+  runtimeCompiler: true,
 };
