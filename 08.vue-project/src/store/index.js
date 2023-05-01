5077463272e97b57abe8fa3d9d2931fee59060b9
maozp <1654331345@qq.com>
2023-05-01 23:40:30 +0800

    vuex 模块化操作

--- 08.vue-project/src/store/index.js
+++ 08.vue-project/src/store/index.js
@@ -1,17 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import rootModule from "@/store/root.module";
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const files = require.context('./modules', false, /\.js$/)
+files.keys().forEach(key => {
+    // 模块对应的内容
+    let store = files(key).default
+    console.log('store', key)
+    let moduleName = key.replace(/^\.\//, '').replace(/\.js$/, '')
+    console.log('moduleName', moduleName)
+    // 动态添加模块
+    let modules = rootModule.modules = rootModule.modules || {}
+    modules[moduleName] = store
+    modules[moduleName].namespaced = true
 })
+
+let store = new Vuex.Store(rootModule)
+
+export default store;
---
+++ 08.vue-project/src/store/root.module.js
@@ -0,0 +1,9 @@
+export default {
+    state: {
+        root: 'xxx'
+    },
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {}
+}
